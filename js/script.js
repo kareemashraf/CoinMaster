ce470f9e2e59a673c042da5611be7ddfa14ab5a2
Kareem <kareem.ashraf.91@gmail.com>
2018-04-23 19:14:16 +0200

    fix pagination

--- js/script.js
+++ js/script.js
@@ -1,95 +1,108 @@
 $(document).ready(function () {
 
-console.log(fetchdata());
+    console.log(fetchdata());
 
 
 });
 
-function getdata(start, limit){
-	var xhr = new XMLHttpRequest();
+function getdata(start, limit) {
+    var xhr = new XMLHttpRequest();
 
-		xhr.open("GET", "https://api.coinmarketcap.com/v1/ticker/?start="+start+"&limit="+limit, false);
-		xhr.send();
-		
-		return JSON.parse(xhr.response); //city details
+    xhr.open("GET", "https://api.coinmarketcap.com/v1/ticker/?start=" + start + "&limit=" + limit, false);
+    xhr.send();
+
+    return JSON.parse(xhr.response); //city details
 
 }
 
 
-function fetchdata(){
-var start = 0;
-var limit = 10;
+function fetchdata() {
+    var start = 0;
+    var limit = 10;
 
-	var data = getdata(start, limit);
-	var t = $('#table').DataTable({
+    var data = getdata(start, limit);
+    var t = $('#table').DataTable({
         'iDisplayLength': -1,
-        "bPaginate": false
+        "bPaginate": false,
+        "aaSorting": [[1, "asc"]],
+        "columnDefs": [
+            {className: "coin", "targets": [0]}
+        ]
     });
 
 
-		for (var i = 0; i< data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
 
-			t.row.add( [
+        t.row.add([
+            "<img id='coin-icon' src='https://coinmark.co/assets/extension/coins/" + data[i].id + ".png'>" + " " + data[i].name,
             data[i].rank,
-            "<img id='coin-icon' src='https://coinmark.co/assets/extension/coins/"+data[i].id+".png'>"+" "+data[i].name,
             data[i].price_usd,
-            data[i].percent_change_1h,
-            data[i].percent_change_24h,
-            data[i].percent_change_7d
-        ] ).draw( false );
+            "<div class='hour" + i + "'>" + data[i].percent_change_1h + "%</div>",
+            "<div class='day" + i + "'>" + data[i].percent_change_24h + "%</div>",
+            "<div class='week" + i + "'>" + data[i].percent_change_7d + "%</div>"
+        ]).draw(false);
 
-			// $("#tbody").append("<tr id='"+data[i].rank+"'>");
-   //          $("#tbody #"+data[i].rank+"").append("<td>"+data[i].rank+"</td>");
-			// $("#tbody #"+data[i].rank+"").append("<td><img id='coin-icon' src='https://coinmark.co/assets/extension/coins/"+data[i].id+".png'> "+' '+data[i].name+"</td>");
-   //          $("#tbody #"+data[i].rank+"").append("<td>"+data[i].price_usd+" $</td>");
+        if (data[i].percent_change_1h > 0) {
+            $(".hour" + i).addClass("green");
+        } else {
+            $(".hour" + i).addClass("red");
+        }
 
-   //          if (data[i].percent_change_1h > 0){
-   //              $("#tbody #"+data[i].rank+"").append("<td><div style='color: green'>"+data[i].percent_change_1h+"%</div></td>");
-			// }else{
-   //              $("#tbody #"+data[i].rank+"").append("<td><div style='color: red'>"+data[i].percent_change_1h+"%</div></td>");
-			// }
+        if (data[i].percent_change_24h > 0) {
+            $(".day" + i).addClass("green");
+        } else {
+            $(".day" + i).addClass("red");
+        }
 
-   //          if (data[i].percent_change_24h > 0){
-   //              $("#tbody #"+data[i].rank+"").append("<td><div style='color: green'>"+data[i].percent_change_24h+"%</div></td>");
-   //          }else{
-   //              $("#tbody #"+data[i].rank+"").append("<td><div style='color: red'>"+data[i].percent_change_24h+"%</div></td>");
-   //          }
+        if (data[i].percent_change_7d > 0) {
+            $(".week" + i).addClass("green");
+        } else {
+            $(".week" + i).addClass("red");
+        }
 
-   //          if (data[i].percent_change_7d > 0){
-   //              $("#tbody #"+data[i].rank+"").append("<td>"+data[i].percent_change_7d+"%</td>");
-   //          }else{
-   //              $("#tbody #"+data[i].rank+"").append("<td>"+data[i].percent_change_7d+"%</td>");
-   //          }
-			// $("#tbody").append("</tr>");
-		}
+    }
 
 
-
-
-    $("#load-more").click(function(){
+    $("#load-more").click(function () {
         start = start + 10;
 
         var data = getdata(start, limit);
 
 
-        for (var i = 0; i< data.length; i++) {
+        for (var i = 0; i < data.length; i++) {
 
-            t.row.add( [
-            data[i].rank,
-            "<img id='coin-icon' src='https://coinmark.co/assets/extension/coins/"+data[i].id+".png'>"+" "+data[i].name,
-            data[i].price_usd,
-            data[i].percent_change_1h,
-            data[i].percent_change_24h,
-            data[i].percent_change_7d
-        ] ).draw( false );
+            t.row.add([
+                "<img id='coin-icon' src='https://coinmark.co/assets/extension/coins/" + data[i].id + ".png'>" + " " + data[i].name,
+                data[i].rank,
+                data[i].price_usd,
+                "<div class='hour" + i + "'>" + data[i].percent_change_1h + "%</div>",
+                "<div class='day" + i + "'>" + data[i].percent_change_24h + "%</div>",
+                "<div class='week" + i + "'>" + data[i].percent_change_7d + "%</div>"
+            ]).draw(false);
+
+            if (data[i].percent_change_1h > 0) {
+                $(".hour" + i).addClass("green");
+            } else {
+                $(".hour" + i).addClass("red");
+            }
+
+            if (data[i].percent_change_24h > 0) {
+                $(".day" + i).addClass("green");
+            } else {
+                $(".day" + i).addClass("red");
+            }
+
+            if (data[i].percent_change_7d > 0) {
+                $(".week" + i).addClass("green");
+            } else {
+                $(".week" + i).addClass("red");
+            }
 
         }
-
 
 
     });
 
 
-	
 }
 
