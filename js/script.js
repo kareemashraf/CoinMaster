773fae986b571df52c867a57962d92809ef4a37b
Kareem <kareem.ashraf.91@gmail.com>
2021-11-16 15:47:59 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
 $(document).ready(function () {
+    $('#loading').append(" <img id='loading' src='img/loader.gif'   >");
 
+    setTimeout(function () {
+        $('#loading').addClass('hidden');
+        fetchdata(null);
+        // add currencies
+        curr();
+    }, 300);
 
-    fetchdata(null);
-
-    // add currencies
-    curr();
 
     $(document).on('change', 'select', curchange);
 
-
 });
 
-function getdata(currency, page, start, limit ) {
+function getdata(currency, page) {
 
-    // var currency = $(".currency").val();
-    console.log('page = ',page);
+    console.log('page = ', page);
 
     var xhr = new XMLHttpRequest();
 
     // xhr.open("GET", "https://pro-api.coinmarketcap.com/v1/ticker/?start=" + start + "&limit=" + limit + "&convert=" + currency, false);
-    xhr.open("GET", "https://api.coingecko.com/api/v3/coins/markets?vs_currency="+currency+"&order=market_cap_desc&per_page=100&page="+page+"&sparkline=false", false);
+    xhr.open("GET", "https://api.coingecko.com/api/v3/coins/markets?vs_currency=" + currency + "&order=market_cap_desc&per_page=100&page=" + page + "&sparkline=false", false);
     xhr.send();
-
+    console.log('sssss:', xhr.readyState);
     return JSON.parse(xhr.response); //city details
 
 }
@@ -50,29 +51,44 @@ function fetchdata(currency) {
             "header": true
         }
     });
-    $('table.dataTable thead .sorting_asc').css('background-image', 'none' );
 
-    $( "#table tbody tr" ).mouseover(function() {
-          $(this).addClass("highlight");
-        });
+    $("#table tbody tr").mouseover(function () {
+        $(this).addClass("highlight");
+    });
+
+    render(t, currency, data);
+
+    var clicks = 0;
+    $("#load-more").click(function () {
+        clicks++;
+
+        if (clicks >= 1) {
+            page = page + 1;
+        }
+
+        console.log("clicks= " + clicks + " pages= " + page);
+        var currency = $(".currency").val();
+        var data = getdata(currency, page);
+
+        render(t, currency, data);
+    });
 
 
-    // var price = "price_" + currency.toLowerCase();
+}
 
-
+function render(t, currency, data) {
     for (var i = 0; i < data.length; i++) {
 
         var img_name = (data[i].name).toLowerCase();
-        // img_name = img_name.replace(/ /g, "-");
 
         t.row.add([
-            "<img class='coin-icon ' src='"+data[i].image+"'   >" + " <a target='_blank' href='https://coinmarketcap.com/currencies/"+img_name+"' >" + data[i].name+"</a>",
+            "<img class='coin-icon ' src='" + data[i].image + "'   >" + " <a target='_blank' href='https://coinmarketcap.com/currencies/" + img_name + "' >" + data[i].name + "</a>",
             data[i].market_cap_rank,
-           symbol(currency)+" "+data[i].current_price+'',
-            "<div class='hour" + i + "'>" +symbol(currency)+ data[i].market_cap + "</div>",
+            symbol(currency) + " " + data[i].current_price + '',
+            "<div class='hour" + i + "'>" + symbol(currency) + data[i].market_cap + "</div>",
             "<div class='day" + i + "'>" + data[i].price_change_percentage_24h + "%</div>",
             "<div class='week" + i + "'>" + data[i].ath_change_percentage + "%</div>",
-            "<a href='https://changelly.com/widget/v1?auth=email&from=usd&to="+data[i].symbol+"' target='_blank'><button type='button'  class='btn btn-primary btn-xs'>Buy</button> </a> | <a href='https://changelly.com/widget/v1?auth=email&from="+data[i].symbol+"&to=usd' target='_blank' ><button type='button' class='btn btn-danger btn-xs'>Sell</button></a>"
+            "<a href='https://changelly.com/widget/v1?auth=email&from=usd&to=" + data[i].symbol + "' target='_blank'><button type='button'  class='btn btn-primary btn-xs'>Buy</button> </a> | <a href='https://changelly.com/widget/v1?auth=email&from=" + data[i].symbol + "&to=usd' target='_blank' ><button type='button' class='btn btn-danger btn-xs'>Sell</button></a>"
         ]).draw(false);
 
 
@@ -89,56 +105,6 @@ function fetchdata(currency) {
         }
 
     }
-
-    var clicks = 0;
-    $("#load-more").click(function () {
-        clicks++;
-
-        if (clicks >= 1) {
-            page = page + 1;
-        }
-        // else {
-        //     page = page;
-        // }
-
-        console.log("clicks= "+clicks + " pages= " + page);
-        var currency = $(".currency").val();
-        var data = getdata(currency, page);
-
-
-        for (var i = 0; i < data.length; i++) {
-
-            var img_name = (data[i].name).toLowerCase();
-            img_name = img_name.replace(/ /g, "-");
-
-            t.row.add([
-                "<img class='coin-icon' src='"+data[i].image+"'   >" + " <a target='_blank' href='https://coinmarketcap.com/currencies/"+img_name+"' >" + data[i].name+"</a>",
-                data[i].market_cap_rank,
-                symbol(currency)+" "+data[i].current_price+'',
-                "<div class='hour" + i + "'>" +symbol(currency)+ data[i].market_cap + "</div>",
-                "<div class='day" + i + "'>" + data[i].price_change_percentage_24h + "%</div>",
-                "<div class='week" + i + "'>" + data[i].ath_change_percentage + "%</div>",
-                "<a href='https://changelly.com/widget/v1?auth=email&from=usd&to="+data[i].symbol+"' target='_blank'><button type='button'  class='btn btn-primary btn-xs'>Buy</button> </a> | <a href='https://changelly.com/widget/v1?auth=email&from="+data[i].symbol+"&to=usd' target='_blank'><button type='button' class='btn btn-danger btn-xs'>Sell</button></a>"
-            ]).draw(false);
-
-            if (data[i].price_change_percentage_24h > 0) {
-                $(".day" + i).addClass("green");
-            } else {
-                $(".day" + i).addClass("red");
-            }
-
-            if (data[i].ath_change_percentage > 0) {
-                $(".week" + i).addClass("green");
-            } else {
-                $(".week" + i).addClass("red");
-            }
-
-        }
-
-
-    });
-
-
 }
 
 
@@ -172,40 +138,43 @@ function symbol(currency) {
 
 function curr() {
 
+
     $("#table_length").append('<select class="form-control currency" ></select>');
+    $("#table_length").append(donate());
 
     var array = ['<option value="USD">USD</option>',
-        '<option value="AUD">AUD</option>',
-        '<option value="BRL">BRL</option>',
-        '<option value="CAD">CAD</option>',
-        '<option value="CHF">CHF</option>',
-        '<option value="CLP">CLP</option>',
-        '<option value="CNY">CNY</option>',
-        '<option value="CZK">CZK</option>',
-        '<option value="DKK">DKK</option>',
+        // '<option value="AUD">AUD</option>',
+        // '<option value="BRL">BRL</option>',
+        // '<option value="CAD">CAD</option>',
+        // '<option value="CHF">CHF</option>',
+        // '<option value="CLP">CLP</option>',
+        // '<option value="CNY">CNY</option>',
+        // '<option value="CZK">CZK</option>',
+        // '<option value="DKK">DKK</option>',
         '<option value="EUR">EUR</option>',
         '<option value="GBP">GBP</option>',
-        '<option value="HKD">HKD</option>',
-        '<option value="HUF">HUF</option>',
-        '<option value="IDR">IDR</option>',
-        '<option value="ILS">ILS</option>',
-        '<option value="INR">INR</option>',
-        '<option value="JPY">JPY</option>',
-        '<option value="KRW">KRW</option>',
-        '<option value="MXN">MXN</option>',
-        '<option value="MYR">MYR</option>',
-        '<option value="NOK">NOK</option>',
-        '<option value="NZD">NZD</option>',
-        '<option value="PHP">PHP</option>',
-        '<option value="PKR">PKR</option>',
-        '<option value="PLN">PLN</option>',
-        '<option value="RUB">RUB</option>',
-        '<option value="SEK">SEK</option>',
-        '<option value="SGD">SGD</option>',
-        '<option value="THB">THB</option>',
-        '<option value="TRY">TRY</option>',
-        '<option value="TWD">TWD</option>',
-        '<option value="ZAR">ZAR</option>'];
+        // '<option value="HKD">HKD</option>',
+        // '<option value="HUF">HUF</option>',
+        // '<option value="IDR">IDR</option>',
+        // '<option value="ILS">ILS</option>',
+        // '<option value="INR">INR</option>',
+        // '<option value="JPY">JPY</option>',
+        // '<option value="KRW">KRW</option>',
+        // '<option value="MXN">MXN</option>',
+        // '<option value="MYR">MYR</option>',
+        // '<option value="NOK">NOK</option>',
+        // '<option value="NZD">NZD</option>',
+        // '<option value="PHP">PHP</option>',
+        // '<option value="PKR">PKR</option>',
+        // '<option value="PLN">PLN</option>',
+        // '<option value="RUB">RUB</option>',
+        // '<option value="SEK">SEK</option>',
+        // '<option value="SGD">SGD</option>',
+        // '<option value="THB">THB</option>',
+        // '<option value="TRY">TRY</option>',
+        // '<option value="TWD">TWD</option>',
+        // '<option value="ZAR">ZAR</option>'
+    ];
 
 
     for (var i = 0; i <= array.length; i++) {
@@ -230,6 +199,21 @@ function curchange() {
 
 function imgError() {
     alert(this.src);
+}
+
+function donate() {
+    var donate = '<div id="donation-up" class="donate"> <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">\n' +
+        '                <input type="hidden" name="cmd" value="_donations">\n' +
+        '                <input type="hidden" name="business" value="kareem.ashraf.91@gmail.com">\n' +
+        '                <input type="hidden" name="lc" value="US">\n' +
+        '                <input type="hidden" name="item_name" value="\'your contribution is always appreciated\' -Kareem Essawy">\n' +
+        '                <input type="hidden" name="no_note" value="0">\n' +
+        '                <input type="hidden" name="currency_code" value="EUR">\n' +
+        '                <input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_SM.gif:NonHostedGuest">\n' +
+        '                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">\n' +
+        '                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">\n' +
+        '            </form></div>'
+    return donate;
 }
 
 
